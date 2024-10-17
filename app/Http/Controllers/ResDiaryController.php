03a7e7c36919cf5ba3f1b71f4969e050eb7bc684
Alex Jeffers <alex@gluestudio.co.uk>
2024-10-17 20:34:40 +0100

    added resdiary controller

--- app/Http/Controllers/ResDiaryController.php
+++ app/Http/Controllers/ResDiaryController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ResDiaryController extends Controller
 {
     public function install(Request $request)
     {
         $codeVerifier = $this->generateCodeVerifier();
+        session()->save($codeVerifier);
         $codeChallenge = $this->generateCodeChallenge($codeVerifier);
 // Get all query parameters
         $data = $request->query();
@@ -18,48 +20,28 @@ class ResDiaryController extends Controller
             $authorizationUri = parse_url($data['authorization_uri'], PHP_URL_QUERY);
             parse_str($authorizationUri, $authParams);
 
-            $data['authorization_uri_params'] = $authParams;
         }
 
-//        dd($data);
 
         return redirect($data['authorization_uri'] . '&code_challenge=' . $codeChallenge . '&code_challenge_method=S256');
 
-//        $client_id = env('RESDIARY_CLIENT_ID');
-//        $redirect_uri = env('RESDIARY_REDIRECT_URI');
-//        $scope = env('RESDIARY_SCOPE');
-//        $state = env('RESDIARY_STATE');
-//        $url = "https://resdiary.com/oauth/authorize?client_id=$client_id&redirect_uri=$redirect_uri&scope=$scope&state=$state";
-//        return redirect($url);
+
     }
 
     public function callback(Request $request)
     {
-        dd($request);
-        $code = $request->code;
-        $client_id = env('RESDIARY_CLIENT_ID');
-        $client_secret = env('RESDIARY_CLIENT_SECRET');
-        $redirect_uri = env('RESDIARY_REDIRECT_URI');
-        $url = "https://resdiary.com/oauth/token";
-        $data = [
+
+        $response = Http::post(env('RESDIARY_OAUTH_URL'), [
             'grant_type' => 'authorization_code',
-            'code' => $code,
-            'client_id' => $client_id,
-            'client_secret' => $client_secret,
-            'redirect_uri' => $redirect_uri
-        ];
-        $response = Http::post($url, $data);
-        $response = json_decode($response->body());
-        $access_token = $response->access_token;
-        $refresh_token = $response->refresh_token;
-        $expires_in = $response->expires_in;
-        $expires_at = time() + $expires_in;
-        $resdiary = new ResDiary();
-        $resdiary->access_token = $access_token;
-        $resdiary->refresh_token = $refresh_token;
-        $resdiary->expires_at = $expires_at;
-        $resdiary->save();
-        return redirect()->route('dashboard');
+            'code' => $request->code,
+            'client_id' => env('RESDIARY_CLIENT_ID'),
+            'client_secret' => env('RESDIARY_CLIENT_SECRET'),
+            'redirect_uri' => env('APP_URL'),
+            'code_verifier' => session()->get('code_verifier')
+        ]);
+
+        dd($response);
+
     }
 
 
