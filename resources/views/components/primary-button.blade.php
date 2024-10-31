e704e15d707152dd6d2247cd3122e158ae0df686
Alex Jeffers <alex@gluestudio.co.uk>
2024-10-31 08:42:02 +0000

    button font changed

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-8 py-1 bg-black text-white grandstander uppercase  rounded-full font-bold text-lg ']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-8 py-1 bg-black text-white font-sans uppercase  rounded-full font-bold text-lg ']) }}>
     {{ $slot }}
 </button>
