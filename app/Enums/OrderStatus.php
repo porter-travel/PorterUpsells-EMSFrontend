7f176c9ae0af74def9b40b8423c2a433a9589ad5
Alex Jeffers <alex@gluestudio.co.uk>
2024-11-17 11:13:55 +0000

    fix for order status table

--- app/Enums/OrderStatus.php
+++ app/Enums/OrderStatus.php
@@ -3,19 +3,22 @@
 namespace App\Enums;
 
 use Illuminate\Validation\Rules\Enum;
+use function Laravel\Prompts\select;
 
 class OrderStatus extends Enum
 {
     const COMPLETE = 'complete';
     const FULFILLED = 'fulfilled';
     const CANCELLED = 'cancelled';
+    const PENDING = 'pending';
 
     public static function getValues(): array
     {
         return [
             self::COMPLETE,
             self::FULFILLED,
-            self::CANCELLED
+            self::CANCELLED,
+            self::PENDING
         ];
     }
 }
