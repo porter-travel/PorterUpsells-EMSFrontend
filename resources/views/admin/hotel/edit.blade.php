68f43ca3493985819da19366465236f9a5bb249f
Alex Jeffers <alex@gluestudio.co.uk>
2025-01-29 14:17:11 +0000

    removed coming soon from add products

--- resources/views/admin/hotel/edit.blade.php
+++ resources/views/admin/hotel/edit.blade.php
@@ -64,16 +64,17 @@
                                             href="/admin/hotel/{{$hotel->id}}/product/create/standard">Standard
                                             Product</a></li>
                                     <li class="pl-12 border-b border-darkGrey py-2">
+                                        <a href="/admin/hotel/{{$hotel->id}}/product/create/calendar">
+                                            Calendar Product
+                                        </a>
+                                    </li>
+                                    <li class="pl-12 border-b border-darkGrey py-2">
                                         <a href="/admin/hotel/{{$hotel->id}}/product/create/restaurant">
                                             <span style="color: grey">Restaurant
                                             Booking (coming soon)</span>
                                         </a>
                                     </li>
-                                    <li class="pl-12 border-b border-darkGrey py-2">
-                                        <a href="/admin/hotel/{{$hotel->id}}/product/create/calendar">
-                                            <span style="color: grey">Calendar Product (coming soon)</span>
-                                        </a>
-                                    </li>
+
 
                                 </ul>
                             </div>
