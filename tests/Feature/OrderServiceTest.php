7247f1ed221df9b6a39f380ae1cf2ebf3839ddfd
Alex Jeffers <alex@gluestudio.co.uk>
2025-02-12 14:52:57 +0000

    updated some tests, maybe fix something

--- tests/Feature/OrderServiceTest.php
+++ tests/Feature/OrderServiceTest.php
@@ -100,4 +100,71 @@ class OrderServiceTest extends TestCase
         $this->assertCount(1, $orderArray);
 
     }
+
+    public function test_it_will_get_orders_by_hotel_for_next_seven_days(){
+        $user = User::factory()->create();
+        $hotel = Hotel::factory()->create([
+            'user_id' => $user->id,
+        ]);
+        $product = Product::factory()->create([
+            'hotel_id' => $hotel->id,
+        ]);
+        $variation = Variation::factory()->create(
+            ['price' => 100, 'product_id' => $product->id]
+        );
+
+        //Create an arrival date of today plus three days
+
+        $arrivalDate = date('Y-m-d', strtotime('+3 days'));
+
+        $booking = Booking::factory()->create([
+            'hotel_id' => $hotel->id,
+            'arrival_date' => $arrivalDate,
+        ]);
+
+        $order = Order::factory()->create([
+            'hotel_id' => $hotel->id,
+            'booking_id' => $booking->id,
+            'payment_status' => 'paid',
+            'arrival_date' => $booking->arrival_date,
+        ]);
+
+        $orderItems = OrderItem::factory()->create([
+            'order_id' => $order->id,
+            'product_id' => $product->id,
+            'variation_id' => $variation->id,
+            'quantity' => 1,
+        ]);
+
+        //Create an arrival date of today plus 10 days
+
+        $arrivalDate = date('Y-m-d', strtotime('+10 days'));
+
+        $booking = Booking::factory()->create([
+            'hotel_id' => $hotel->id,
+            'arrival_date' => $arrivalDate,
+        ]);
+
+        $order = Order::factory()->create([
+            'hotel_id' => $hotel->id,
+            'booking_id' => $booking->id,
+            'payment_status' => 'paid',
+            'arrival_date' => $booking->arrival_date,
+        ]);
+
+        $orderItems = OrderItem::factory()->create([
+            'order_id' => $order->id,
+            'product_id' => $product->id,
+            'variation_id' => $variation->id,
+            'quantity' => 1,
+            'date' => $booking->arrival_date,
+        ]);
+
+
+        $service = new OrderService();
+
+        $orderArray = $service->getOrdersByHotelForNextSevenDays($hotel->id);
+
+        dd($orderArray);
+    }
 }
