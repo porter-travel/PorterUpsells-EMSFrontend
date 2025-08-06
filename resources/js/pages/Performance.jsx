15c02cb3923723d17aa8236b180d86536c23e470
gpiazzon <72225531+gpiazzon@users.noreply.github.com>
2025-08-06 08:44:59 +0100

    Revert "feat: add saas-style layout for admin"

--- resources/js/pages/Performance.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Performance() {
-  return <div>Performance Page</div>;
-}
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
@@ -8,10 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/app.jsx',
             ],
             refresh: true,
         }),
-        react(),
     ],
 });
